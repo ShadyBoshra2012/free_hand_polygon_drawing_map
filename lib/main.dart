a5b094e25b8e5761582b90661d3346efc70bd072
Shady Boshra <shady_alkrar@yahoo.com>
2020-12-20 22:56:12 +0200

    Init Commit

---
+++ lib/main.dart
@@ -0,0 +1,168 @@
+import 'dart:async';
+import 'dart:collection';
+import 'dart:io';
+import 'dart:math' as Math;
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Free Hand Polygon Drawing',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static final Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  final Set<Polygon> _polygons = HashSet<Polygon>();
+  final Set<Polyline> _polyLines = HashSet<Polyline>();
+
+  bool _drawPolygonEnabled = false;
+  List<LatLng> _userPolyLinesLatLngList = List();
+  bool _clearDrawing = false;
+  int _lastXCoordinate, _lastYCoordinate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onPanUpdate: (_drawPolygonEnabled) ? _onPanUpdate : null,
+        onPanEnd: (_drawPolygonEnabled) ? _onPanEnd : null,
+        child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+          polygons: _polygons,
+          polylines: _polyLines,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleDrawing,
+        tooltip: 'Drawing',
+        child: Icon((_drawPolygonEnabled) ? Icons.cancel : Icons.edit),
+      ),
+    );
+  }
+
+  _toggleDrawing() {
+    _clearPolygons();
+    setState(() => _drawPolygonEnabled = !_drawPolygonEnabled);
+  }
+
+  _onPanUpdate(DragUpdateDetails details) async {
+    // To start draw new polygon every time.
+    if (_clearDrawing) {
+      _clearDrawing = false;
+      _clearPolygons();
+    }
+
+    if (_drawPolygonEnabled) {
+      double x, y;
+      if (Platform.isAndroid) {
+        // It times in 3 without any meaning,
+        // We think it's an issue with GoogleMaps package.
+        x = details.globalPosition.dx * 3;
+        y = details.globalPosition.dy * 3;
+      } else if (Platform.isIOS) {
+        x = details.globalPosition.dx;
+        y = details.globalPosition.dy;
+      }
+
+      // Round the x and y.
+      int xCoordinate = x.round();
+      int yCoordinate = y.round();
+
+      // Check if the distance between last point is not too far.
+      // to prevent two fingers drawing.
+      if (_lastXCoordinate != null && _lastYCoordinate != null) {
+        var distance = Math.sqrt(Math.pow(xCoordinate - _lastXCoordinate, 2) + Math.pow(yCoordinate - _lastYCoordinate, 2));
+        // Check if the distance of point and point is large.
+        if (distance > 80.0) return;
+      }
+
+      // Cached the coordinate.
+      _lastXCoordinate = xCoordinate;
+      _lastYCoordinate = yCoordinate;
+
+      ScreenCoordinate screenCoordinate = ScreenCoordinate(x: xCoordinate, y: yCoordinate);
+
+      final GoogleMapController controller = await _controller.future;
+      LatLng latLng = await controller.getLatLng(screenCoordinate);
+
+      try {
+        // Add new point to list.
+        _userPolyLinesLatLngList.add(latLng);
+
+        _polyLines.removeWhere((polyline) => polyline.polylineId.value == 'user_polyline');
+        _polyLines.add(
+          Polyline(
+            polylineId: PolylineId('user_polyline'),
+            points: _userPolyLinesLatLngList,
+            width: 2,
+            color: Colors.blue,
+          ),
+        );
+      } catch (e) {
+        print(" error painting $e");
+      }
+      setState(() {});
+    }
+  }
+
+  _onPanEnd(DragEndDetails details) async {
+    // Reset last cached coordinate
+    _lastXCoordinate = null;
+    _lastYCoordinate = null;
+
+    if (_drawPolygonEnabled) {
+      _polygons.removeWhere((polygon) => polygon.polygonId.value == 'user_polygon');
+      _polygons.add(
+        Polygon(
+          polygonId: PolygonId('user_polygon'),
+          points: _userPolyLinesLatLngList,
+          strokeWidth: 2,
+          strokeColor: Colors.blue,
+          fillColor: Colors.blue.withOpacity(0.4),
+        ),
+      );
+      setState(() {
+        _clearDrawing = true;
+      });
+    }
+  }
+
+  _clearPolygons() {
+    setState(() {
+      _polyLines.clear();
+      _polygons.clear();
+      _userPolyLinesLatLngList.clear();
+    });
+  }
+}
